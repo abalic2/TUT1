534df00fed959dc5f40c891541b26492494237d3
abalic2 <abalic2@etf.unsa.ba>
2018-10-20 09:11:35 +0200

    Sredjivanje maina

--- src/ba/unsa/etf/rpr/tut1/Main.java
+++ src/ba/unsa/etf/rpr/tut1/Main.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static int sumaCifara(int broj){
-        int temp=broj,suma=0;
+        int temp = broj, suma = 0;
         do{
-            suma+=temp%10;
-            temp/=10;
-        }while(temp>0);
+            suma += temp % 10;
+            temp /= 10;
+        }while(temp > 0);
         return suma;
     }
 
     public static void main(String[] args) {
-	    Scanner ulaz=new Scanner(System.in);
-	    int n=ulaz.nextInt();
-	    for(int i=1;i<n;i++) {
+        System.out.print("Unesite broj n: ");
+	    Scanner ulaz = new Scanner(System.in);
+	    int n = ulaz.nextInt();
+	    System.out.println("Brojevi od 1 do n koji su djeljivi sa sumom svojih cifara su: ");
+	    for(int i = 1; i <= n; i++) {
             if (i % sumaCifara(i) == 0) System.out.println(i);
         }
     }
