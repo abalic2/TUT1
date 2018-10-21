307ba7829609aadae58c6540750e3624d217ac3f
abalic2 <abalic2@etf.unsa.ba>
2018-10-22 01:22:14 +0200

    Dodavanje testova

---
+++ rpr-t1-z4/test/ba/unsa/etf/rpr/tut1/PredmetTest.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr.tut1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PredmetTest {
+
+    @Test
+    void dostignutMaxBrojStudenata() {
+        assertThrows(Exception.class,()->{
+            Predmet p = new Predmet("IM", 125, 2);
+            Student s1=new Student("Amra","Dadic",18215);
+            Student s2=new Student ("Nadja","Sokolovic",18000);
+            Student s3=new Student ("Bakir","Sabljica",18111);
+            p.upisi(s1);
+            p.upisi(s2);
+            p.upisi(s3);
+        });
+    }
+
+}
---
+++ rpr-t1-z4/test/ba/unsa/etf/rpr/tut1/StudentTest.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.tut1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void ispisiStudenta() {
+        Student s=new Student("Dzan","Tabakovic",18044);
+        assertEquals("Tabakovic Dzan (18044)",s.toString());
+    }
+}
